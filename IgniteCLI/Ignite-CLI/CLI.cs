3603b0f33c2a1065f019fd81d13a9c0475bf9f8d
Cody Lee <clee@omegaair.com>
2018-08-02 15:39:30 -0500

    Added `CLI.Run(string input)` for #15 Added Sample project

--- IgniteCLI/Ignite-CLI/CLI.cs
+++ IgniteCLI/Ignite-CLI/CLI.cs
@@ -5,6 +5,12 @@ using System.Linq;
 
 namespace IgniteCLI
 {
+    class InputCommand
+    {
+        public string Name;
+        public Dictionary<string, string> Arguments;
+    }
+
     public class CLI
     {
         #region Convenience Functions
@@ -14,62 +20,67 @@ namespace IgniteCLI
         #endregion
 
         private static CommandList Commands;
+        private static Command HelpCommand = new Command
+        {
+            Name = "help",
+            Description = "Shows this list of commands",
+            Function = args => { CLI.Help(); }
+        };
 
         public static void Start(CommandList commands)
         {
             Commands = commands;
+            if (!Commands.Any(x => x.Name == "help"))
+                Commands.Insert(0, HelpCommand);
+
             Console.OutputEncoding = System.Text.Encoding.Unicode;
             Console.Write("> ");
 
             var input = Console.ReadLine();
             while (input != "exit")
             {
-                string cmd = "help";
-                var cmdArgs = new Dictionary<string, string>();
-                if (input.Contains(" "))
-                {
-                    try
-                    {
-                        cmd = input.Substring(0, input.IndexOf(" "));
-                        var tokens = input.Substring(input.IndexOf(" ") + 2).Split(new string[] { " -" }, StringSplitOptions.RemoveEmptyEntries);
-                        foreach (var t in tokens)
-                        {
-                            if (t.Contains(' '))
-                                cmdArgs.Add(t.Substring(0, t.IndexOf(' ')).ToLower(), t.Substring(t.IndexOf(' ') + 1));
-                            else
-                                cmdArgs.Add(t, "true");
-                        }
-                    }
-                    catch { }
-                }
-                else cmd = input;
+                InputCommand cmd = ParseInput(input);
 
                 if (input.Length != 0)
                 {
-                    Stopwatch sw = new Stopwatch();
-                    sw.Start();
-
-                    Command exec = Commands[cmd.ToLower()] ?? Commands["help"];
-                    if (!exec.RequiredArgs.Select(x => x.Tag).All(x => cmdArgs.ContainsKey(x)))
-                        exec = Commands["help"];
-                    try
-                    {
-                        exec.Function.Invoke(cmdArgs);
-                        sw.Stop();
-                        CLI.Out(sw.ElapsedMilliseconds + "ms");
-                    }
-                    catch (Exception e)
-                    {
-                        sw.Stop();
-                        CLI.Out(e.Message, ConsoleColor.Red);
-                        CLI.Help();
-                    }
-
+                    Run(cmd);
                     CLI.Out();
                 }
                 Console.Write("> ");
                 input = Console.ReadLine();
             }
+        }
+
+        private static InputCommand ParseInput(string input)
+        {
+            string cmd = "help";
+            var cmdArgs = new Dictionary<string, string>();
+            if (input.Contains(" "))
+            {
+                try
+                {
+                    cmd = input.Substring(0, input.IndexOf(" "));
+                    var tokens = input.Substring(input.IndexOf(" ") + 2).Split(new string[] { " -" }, StringSplitOptions.RemoveEmptyEntries);
+                    foreach (var t in tokens)
+                    {
+                        if (t.Contains(' '))
+                            cmdArgs.Add(t.Substring(0, t.IndexOf(' ')).ToLower(), t.Substring(t.IndexOf(' ') + 1));
+                        else
+                            cmdArgs.Add(t, "true");
+                    }
+                }
+                catch { }
+            }
+            else
+            {
+                cmd = input;
+            }
+
+            return new InputCommand
+            {
+                Name = cmd,
+                Arguments = cmdArgs
+            };
         }
 
         public static void Break() => Out("-----------------------------------------------------", ConsoleColor.Green);
@@ -100,6 +111,30 @@ namespace IgniteCLI
             Break();
         }
 
+        private static void Run(InputCommand cmd)
+        {
+            Command exec = Commands[cmd.Name.ToLower()] ?? Commands["help"];
+            if (!exec.RequiredArgs.Select(x => x.Tag).All(x => cmd.Arguments.ContainsKey(x)))
+                exec = Commands["help"];
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            try
+            {
+                exec.Function.Invoke(cmd.Arguments);
+                sw.Stop();
+                CLI.Out(sw.ElapsedMilliseconds + "ms");
+            }
+            catch (Exception e)
+            {
+                sw.Stop();
+                CLI.Out(e.Message, ConsoleColor.Red);
+                CLI.Help();
+            }
+        }
+
+        public static void Run(string input) => CLI.Run(ParseInput(input));
+
         public static void Run(string command, params string[] args)
         {
             Dictionary<string, string> arguments = new Dictionary<string, string>();
@@ -108,7 +143,7 @@ namespace IgniteCLI
                 var ss = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
                 arguments.Add(ss[0], ss[1]);
             }
-            Commands[command].Function.Invoke(arguments);
+            Run(new InputCommand { Name = command, Arguments = arguments });
         }
 
         public static void Run(params string[] input)
