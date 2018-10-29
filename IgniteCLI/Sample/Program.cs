6c8ab1a044a29150579c8528ae1845ae71de7145
Hamed Saeedi <hameds@gmail.com>
2018-10-29 18:49:49 +0330

    rename CommandList to Commands (#41)
    * update gitignore to ignore mac ds_store
    
    * rename CommandList to Commands
    
    resolve #11
    
    * update readme with renamed class
    
    because I changed `CommandList` to `Commands` as you can see in #41

--- IgniteCLI/Sample/Program.cs
+++ IgniteCLI/Sample/Program.cs
@@ -7,7 +7,7 @@ namespace Sample
     {
         static void Main(string[] a)
         {
-            CLI.Start(new CommandList
+            CLI.Start(new Commands
             {
                 new Command
                 {
